188daacee3fdf2416d2e1e157c9fd996b9b1f9ff
lordrukie <lordrukie@localhost.localdomain>
2021-08-17 09:20:51 +0900

    First Deploy :zap::zap

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+dir="/tmp/keyboard-switch/"
+regexDir="\/tmp\/keyboard-swtich\/" # Equal to /opt/keyboard-switch/
+
+mkdir -p $dir
+cp key-disable.png $dir
+cp key-enable.png $dir
+
+cp keyboard-switch.sh $dir
+cp keyboard-switch.desktop $dir
+
+sed -i "s/\/path\//$regexDir/g" $dir/keyboard-switch.desktop
+sed -i "s/\/path\//$regexDir/g" $dir/keyboard-switch.sh
+
+chmod +x $dir/keyboard-switch.sh # Adding executable bit so it can be executed
